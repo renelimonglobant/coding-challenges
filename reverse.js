ba39edceb5439231396885da69d349395a57ea81
renelimonglobant <rene.limon@globant.com>
2024-01-09 13:34:00 -0600

    add reverse script tha match january 9th challenge

---
+++ reverse.js
@@ -0,0 +1,10 @@
+function reverse(a) {
+  const reversed = a[0] === '-' ? '-' + reverseStringNumber(a.substring(1)) : reverseStringNumber(a)
+  if (Number(reversed).toString(2).length > 32) return 0
+  return Number(reversed)
+}
+
+const reverseStringNumber = (value) => value.split('').reverse().join('').replace(/^0+/, '')
+
+const result = reverse(process.argv[2])
+console.log(result)
